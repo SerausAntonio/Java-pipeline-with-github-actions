23b845051609662002749ad24dd9828c0db26ef1
Antonio Seraus <aseraus@chello.nl>
2025-07-29 22:56:33 +0200

    first commit

---
+++ src/test/java/GoogleSearchTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class GoogleSearchTest {
+    public WebDriver driver;
+    public WebDriverWait wait;
+    @Test
+    public void searchTest(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.google.com/?hl=en&gl=US&acceptLang=nl");
+        Assert.assertEquals(driver.getTitle(),"Google");
+        driver.findElement(By.xpath("//*[text()='Accept all']")).click();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.name("q"))));
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("selenium");
+        searchBox.sendKeys(Keys.RETURN);
+        driver.quit();
+    }
+}
